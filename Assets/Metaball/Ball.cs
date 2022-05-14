40ddd02912d191fd62d8b4508241b2c9f8079f58
Jona <jonalukas@web.de>
2022-05-14 12:23:29 +0200

    Initial commit, including metaball and marching cubes from hands on session

---
+++ Assets/Metaball/Ball.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball
+{
+    public float R;
+    public Vector3 position;
+    public Ball(float r, Vector3 pos)
+    {
+        R = r;
+        position = pos;
+    }
+
+    public float Value(float x, float y, float z)
+    {
+        //TODO implement metaball function (Aufgabe 1)
+        //useful functions: Mathf.Pow(), Mathf.Exp()
+
+        // polynomial solution
+        float r = Mathf.Sqrt(Mathf.Pow(x - position.x, 2) + Mathf.Pow(y - position.y, 2) + Mathf.Pow(z - position.z, 2));
+        return Mathf.Pow(R, 2f)/Mathf.Pow(r, 2f);
+
+        // exponential solution
+        //return Mathf.Exp(0.5f - (0.5f * (Mathf.Pow(x - position.x, 2) + Mathf.Pow(y - position.y, 2) + Mathf.Pow(z - position.z, 2))) / (R*R));
+    }
+}

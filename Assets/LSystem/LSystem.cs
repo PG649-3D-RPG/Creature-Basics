4a8a8396f013b8dc5824a27ff906be0a6f05f7c8
kb1t <kb1t@users.noreply.github.com>
2022-05-26 14:47:14 +0200

    introduced a namespace implemented initial direction option

--- Assets/LSystem/LSystem.cs
+++ Assets/LSystem/LSystem.cs
@@ -3,229 +3,240 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-// TODO: Houdini Syntax; in grammatik variable länge erlauben
-// https://www.sidefx.com/docs/houdini/nodes/sop/lsystem.html
-public class LSystem
+namespace LSystem
 {
-    private readonly int default_dist;
-    private readonly short default_angle;
-
-    private readonly uint default_cross_sections;
-    private readonly uint default_cross_section_divisions;
-
-    /// <summary>
-    /// An example for the output of the L-System.
-    /// </summary>
-    public static readonly List<Tuple<Vector3, Vector3>> EXAMPLE = new() { new(Vector3.zero, new(0f, 10f, 0f)), new(new(0f, 10f, 0f), new(0f, 20f, 0f)), new(new(0f, 20f, 0f), new(-10f, 20f, 0f)), new(new(0f, 20f, 0f), new(10f, 20f, 0f)), new(new(0f, 20f, 0f), new(0f, 30f, 0f)), new(new(0f, 30f, 0f), new(-5f, 38.66f, 0f)), new(new(0f, 30f, 0f), new(5f, 38.66f, 0f)) };
-    /// <summary>
-    /// terminal symbols of the l-system
-    /// </summary>
-    public static readonly List<char> TERMINALS = new() { 'F', '+', '-', '&', '^', '\\', '/', '|', '*', '[', ']' };
-
-    /// <summary>
-    /// Initializes an L-System.
-    /// </summary>
-    /// <param name="d">Default forward distance of the turtle.</param>
-    /// <param name="a">Default angle of the turtle</param>
-    /// <param name="cs">Default number of cross sections for the drawing.</param>
-    /// <param name="csd">Default number of cross section divisions for the drawing.</param>
-    public LSystem(int d = 10, short a = 90, uint cs = 4, uint csd = 2)
+    // TODO: Houdini Syntax; in grammatik variable länge erlauben
+    // https://www.sidefx.com/docs/houdini/nodes/sop/lsystem.html
+    public class LSystem
     {
-        default_dist = d;
-        default_angle = a;
-        default_cross_sections = cs;
-        default_cross_section_divisions = csd;
-    }
+        private readonly int default_dist;
+        private readonly short default_angle;
 
-    private Tuple<char, int[]> ParseArguments(ref string str, ref int index, char symbol, int default_value)
-    {
-        Tuple<char, int[]> result;
-        if (index < str.Length - 1 && str[index + 1] == '(')
-        {
-            // search index of ')'
-            var end = index + str[index..].IndexOf(')');
-            var tmp = str[(index + 2)..end];
-            var d = int.Parse(tmp);
-            result = new(symbol, new int[] { d });
-            index = end + 1;
-        }
-        else
-        {
-            result = new(symbol, new int[] { default_value });
-            index++;
-        }
-        return result;
-    }
+        private readonly uint default_cross_sections;
+        private readonly uint default_cross_section_divisions;
 
-    private List<Tuple<char, int[]>> Tokenize(string str)
-    {
-        List<Tuple<char, int[]>> tokens = new();
-        for (int i = 0; i < str.Length;)
+        private readonly Vector3 initial_direction;
+
+        /// <summary>
+        /// An example for the output of the L-System.
+        /// </summary>
+        public static readonly List<Tuple<Vector3, Vector3>> EXAMPLE = new() { new(Vector3.zero, new(0f, 10f, 0f)), new(new(0f, 10f, 0f), new(0f, 20f, 0f)), new(new(0f, 20f, 0f), new(-10f, 20f, 0f)), new(new(0f, 20f, 0f), new(10f, 20f, 0f)), new(new(0f, 20f, 0f), new(0f, 30f, 0f)), new(new(0f, 30f, 0f), new(-5f, 38.66f, 0f)), new(new(0f, 30f, 0f), new(5f, 38.66f, 0f)) };
+        /// <summary>
+        /// terminal symbols of the l-system
+        /// </summary>
+        public static readonly List<char> TERMINALS = new() { 'F', '+', '-', '&', '^', '\\', '/', '|', '*', '[', ']' };
+
+        /// <summary>
+        /// Initializes an L-System.
+        /// </summary>
+        /// <param name="d">Default forward distance of the turtle.</param>
+        /// <param name="a">Default angle of the turtle</param>
+        /// <param name="cs">Default number of cross sections for the drawing.</param>
+        /// <param name="csd">Default number of cross section divisions for the drawing.</param>
+        public LSystem(int d, short a, uint cs, uint csd, INITIAL_DIRECTION init_dir)
         {
-            switch (str[i])
+            default_dist = d;
+            default_angle = a;
+            default_cross_sections = cs;
+            default_cross_section_divisions = csd;
+            initial_direction = init_dir switch
             {
-                case 'F':
-                    tokens.Add(ParseArguments(ref str, ref i, str[i], default_dist));
-                    break;
-                case '+':
-                    tokens.Add(ParseArguments(ref str, ref i, str[i], default_angle));
-                    break;
-                case '-':
-                    tokens.Add(ParseArguments(ref str, ref i, str[i], default_angle));
-                    break;
-                case '&':
-                    tokens.Add(ParseArguments(ref str, ref i, str[i], default_angle));
-                    break;
-                case '^':
-                    tokens.Add(ParseArguments(ref str, ref i, str[i], default_angle));
-                    break;
-                case '\\':
-                    tokens.Add(ParseArguments(ref str, ref i, str[i], default_angle));
-                    break;
-                case '/':
-                    tokens.Add(ParseArguments(ref str, ref i, str[i], default_angle));
-                    break;
-                default:
-                    tokens.Add(new(str[i], new int[0]));
-                    i++;
-                    break;
+                INITIAL_DIRECTION.UP => Vector3.up,
+                INITIAL_DIRECTION.DOWN => Vector3.down,
+                INITIAL_DIRECTION.LEFT => Vector3.left,
+                INITIAL_DIRECTION.RIGHT => Vector3.right,
+                INITIAL_DIRECTION.FORWARD => Vector3.forward,
+                INITIAL_DIRECTION.BACK => Vector3.back,
+                INITIAL_DIRECTION.DIAGONAL => Vector3.one,
+                _ => Vector3.down,
+            };
+        }
+
+        private Tuple<char, int[]> ParseArguments(ref string str, ref int index, char symbol, int default_value)
+        {
+            Tuple<char, int[]> result;
+            if (index < str.Length - 1 && str[index + 1] == '(')
+            {
+                // search index of ')'
+                var end = index + str[index..].IndexOf(')');
+                var tmp = str[(index + 2)..end];
+                var d = int.Parse(tmp);
+                result = new(symbol, new int[] { d });
+                index = end + 1;
             }
-        }
-        return tokens;
-    }
-
-    private double ConvertToRadians(double angle)
-    {
-        return (Math.PI / 180) * angle;
-    }
-
-    private Vector3 CalculateEnd2D(int rot, Vector3 start, int dist)
-    {
-        var x = start.x + dist * (float)Math.Cos(ConvertToRadians(rot));
-        var y = start.y + dist * (float)Math.Sin(ConvertToRadians(rot));
-        return new Vector3(x, y, 0);
-    }
-
-    private Vector3 CalculateEnd3D(Vector3 v, Vector3 start, int dist)
-    {
-        return start + dist * v;
-    }
-
-    private List<Tuple<Vector3, Vector3>> Turtle3D(List<Tuple<char, int[]>> tokens)
-    {
-        Vector3 scarlet_rot = Vector3.down;
-        Vector3 current = Vector3.zero;
-        List<Tuple<Vector3, Vector3>> tuples = new();
-        //LSystemTree<Tuple<Vector3, Vector3>> tuples = new(new(current,current));
-        //var currentNode = tuples;
-        Stack<Tuple<Vector3, Vector3>> st = new();
-        //Stack <Tuple<Vector3, Vector3,LSystemTree<Tuple<Vector3, Vector3>>>> st = new();
-        foreach (var e in tokens)
-        {
-            switch (e.Item1)
+            else
             {
-                case 'F':
-                    var end = CalculateEnd3D(scarlet_rot, current, e.Item2[0]);// new endpoint
-                    tuples.Add(new(current, end));// last endpoint = new startpoint
-                    //currentNode = currentNode.addChild(new(current, end));
-                    current = end;
-                    break;
-                case '+': // turn right
-                    scarlet_rot = Quaternion.Euler(0, 0, e.Item2[0]) * scarlet_rot;
-                    break;
-                case '-': // turn left
-                    scarlet_rot = Quaternion.Euler(0, 0, -e.Item2[0]) * scarlet_rot;
-                    break;
-                case '&': // pitch up
-                    scarlet_rot = Quaternion.Euler(e.Item2[0], 0, 0) * scarlet_rot;
-                    break;
-                case '^': // pitch down
-                    scarlet_rot = Quaternion.Euler(-e.Item2[0], 0, 0) * scarlet_rot;
-                    break;
-                case '\\': // roll clockwise
-                    scarlet_rot = Quaternion.Euler(0, e.Item2[0], 0) * scarlet_rot;
-                    break;
-                case '/': // roll counter-clockwise
-                    scarlet_rot = Quaternion.Euler(0, -e.Item2[0], 0) * scarlet_rot;
-                    break;
-                case '|': // turn 180 deg
-                    scarlet_rot = Quaternion.Euler(0, 0, 180) * scarlet_rot;
-                    break;
-                case '*': // roll 180 deg
-                    scarlet_rot = Quaternion.Euler(0, 180, 0) * scarlet_rot;
-                    break;
-                case '[':
-                    st.Push(new(scarlet_rot, current));
-                    //st.Push(new(scarlet_rot, current,currentNode));
-                    break;
-                case ']':
-                    var tmp = st.Pop();
-                    scarlet_rot = tmp.Item1;
-                    current = tmp.Item2;
-                    //currentNode = tmp.Item3;
-                    break;
-                default:
-                    break;
+                result = new(symbol, new int[] { default_value });
+                index++;
             }
+            return result;
         }
-        return tuples;
-    }
-    private string Parse(string s, uint it, Dictionary<char, string> rules)
-    {
-        string w = s;
-        for (int i = 0; i < it; i++)
+
+        private List<Tuple<char, int[]>> Tokenize(string str)
         {
-            string temp = "";
-            foreach (var c in w)
+            List<Tuple<char, int[]>> tokens = new();
+            for (int i = 0; i < str.Length;)
             {
-                if (rules.ContainsKey(c))
+                switch (str[i])
                 {
-                    // NT
-                    temp += rules[c];
-                }
-                else
-                {
-                    // Terminal
-                    temp += c;
+                    case 'F':
+                        tokens.Add(ParseArguments(ref str, ref i, str[i], default_dist));
+                        break;
+                    case '+':
+                        tokens.Add(ParseArguments(ref str, ref i, str[i], default_angle));
+                        break;
+                    case '-':
+                        tokens.Add(ParseArguments(ref str, ref i, str[i], default_angle));
+                        break;
+                    case '&':
+                        tokens.Add(ParseArguments(ref str, ref i, str[i], default_angle));
+                        break;
+                    case '^':
+                        tokens.Add(ParseArguments(ref str, ref i, str[i], default_angle));
+                        break;
+                    case '\\':
+                        tokens.Add(ParseArguments(ref str, ref i, str[i], default_angle));
+                        break;
+                    case '/':
+                        tokens.Add(ParseArguments(ref str, ref i, str[i], default_angle));
+                        break;
+                    default:
+                        tokens.Add(new(str[i], new int[0]));
+                        i++;
+                        break;
                 }
             }
-            w = temp;
+            return tokens;
         }
-        return w;
-    }
 
-    /// <summary>
-    /// Constructs the result string from the supplied start string and rules for the specified number of iterations and
-    /// returns a list of <start,end> 3D-points that the turtle has drawn.
-    /// </summary>
-    /// <param name="start">The initial string that will be expanded.</param>
-    /// <param name="iterations">The number of iterations that the replacements will be applied for.</param>
-    /// <param name="rules">The replacement rules.</param>
-    /// <param name="printResult">If set to true, will print the list of <start,end> points to the console. Default: false</param>
-    /// <returns>The list of <start,end> 3D-points the turtle has drawn.</returns>
-    public List<Tuple<Vector3, Vector3>> Evaluate(string start, uint iterations, Dictionary<char, string> rules, bool printResult = false)
-    {
-        var expanded = Parse(start, iterations, rules);
-        if (printResult) Debug.Log(expanded);
-        // List<char> non_terminals = new(rules.Keys);
-        var tokens = Tokenize(expanded);
-        PrintList(tokens);
-        var res = Turtle3D(tokens);
-        if (printResult) PrintList(res);
-        return res;
-    }
-
-    /// <summary>
-    /// Prints a list of tuples of 3D Vectors.
-    /// </summary>
-    /// <param name="l">The list that shall be printed.</param>
-    public static void PrintList<T, T2>(List<Tuple<T, T2>> l)
-    {
-        string o = "[  ";
-        foreach (var e in l)
+        private double ConvertToRadians(double angle)
         {
-            o += e.ToString() + "  ";
+            return (Math.PI / 180) * angle;
         }
-        Debug.Log(o + "]");
+
+        private Vector3 CalculateEnd2D(int rot, Vector3 start, int dist)
+        {
+            var x = start.x + dist * (float)Math.Cos(ConvertToRadians(rot));
+            var y = start.y + dist * (float)Math.Sin(ConvertToRadians(rot));
+            return new Vector3(x, y, 0);
+        }
+
+        private Vector3 CalculateEnd3D(Vector3 v, Vector3 start, int dist)
+        {
+            return start + dist * v;
+        }
+
+        private List<Tuple<Vector3, Vector3>> Turtle3D(List<Tuple<char, int[]>> tokens)
+        {
+            Vector3 scarlet_rot = initial_direction;
+            Vector3 current = Vector3.zero;
+            List<Tuple<Vector3, Vector3>> tuples = new();
+            Stack<Tuple<Vector3, Vector3>> st = new();
+            foreach (var e in tokens)
+            {
+                switch (e.Item1)
+                {
+                    case 'F':
+                        var end = CalculateEnd3D(scarlet_rot, current, e.Item2[0]);// new endpoint
+                        tuples.Add(new(current, end));// last endpoint = new startpoint
+                                                      //currentNode = currentNode.addChild(new(current, end));
+                        current = end;
+                        break;
+                    case '+': // turn right
+                        scarlet_rot = Quaternion.Euler(0, 0, e.Item2[0]) * scarlet_rot;
+                        break;
+                    case '-': // turn left
+                        scarlet_rot = Quaternion.Euler(0, 0, -e.Item2[0]) * scarlet_rot;
+                        break;
+                    case '&': // pitch up
+                        scarlet_rot = Quaternion.Euler(e.Item2[0], 0, 0) * scarlet_rot;
+                        break;
+                    case '^': // pitch down
+                        scarlet_rot = Quaternion.Euler(-e.Item2[0], 0, 0) * scarlet_rot;
+                        break;
+                    case '\\': // roll clockwise
+                        scarlet_rot = Quaternion.Euler(0, e.Item2[0], 0) * scarlet_rot;
+                        break;
+                    case '/': // roll counter-clockwise
+                        scarlet_rot = Quaternion.Euler(0, -e.Item2[0], 0) * scarlet_rot;
+                        break;
+                    case '|': // turn 180 deg
+                        scarlet_rot = Quaternion.Euler(0, 0, 180) * scarlet_rot;
+                        break;
+                    case '*': // roll 180 deg
+                        scarlet_rot = Quaternion.Euler(0, 180, 0) * scarlet_rot;
+                        break;
+                    case '[':
+                        st.Push(new(scarlet_rot, current));
+                        break;
+                    case ']':
+                        var tmp = st.Pop();
+                        scarlet_rot = tmp.Item1;
+                        current = tmp.Item2;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return tuples;
+        }
+        private string Parse(string s, uint it, Dictionary<char, string> rules)
+        {
+            string w = s;
+            for (int i = 0; i < it; i++)
+            {
+                string temp = "";
+                foreach (var c in w)
+                {
+                    if (rules.ContainsKey(c))
+                    {
+                        // NT
+                        temp += rules[c];
+                    }
+                    else
+                    {
+                        // Terminal
+                        temp += c;
+                    }
+                }
+                w = temp;
+            }
+            return w;
+        }
+
+        /// <summary>
+        /// Constructs the result string from the supplied start string and rules for the specified number of iterations and
+        /// returns a list of <start,end> 3D-points that the turtle has drawn.
+        /// </summary>
+        /// <param name="start">The initial string that will be expanded.</param>
+        /// <param name="iterations">The number of iterations that the replacements will be applied for.</param>
+        /// <param name="rules">The replacement rules.</param>
+        /// <param name="printResult">If set to true, will print the list of <start,end> points to the console. Default: false</param>
+        /// <returns>The list of <start,end> 3D-points the turtle has drawn.</returns>
+        public List<Tuple<Vector3, Vector3>> Evaluate(string start, uint iterations, Dictionary<char, string> rules, bool printResult = false)
+        {
+            var expanded = Parse(start, iterations, rules);
+            if (printResult) Debug.Log(expanded);
+            // List<char> non_terminals = new(rules.Keys);
+            var tokens = Tokenize(expanded);
+            PrintList(tokens);
+            var res = Turtle3D(tokens);
+            if (printResult) PrintList(res);
+            return res;
+        }
+
+        /// <summary>
+        /// Prints a list of tuples of 3D Vectors.
+        /// </summary>
+        /// <param name="l">The list that shall be printed.</param>
+        public static void PrintList<T, T2>(List<Tuple<T, T2>> l)
+        {
+            string o = "[  ";
+            foreach (var e in l)
+            {
+                o += e.ToString() + "  ";
+            }
+            Debug.Log(o + "]");
+        }
     }
 }
--- Assets/LSystem/LSystemEditor.cs
+++ Assets/LSystem/LSystemEditor.cs
@@ -5,95 +5,97 @@ using System.Linq;
 using MarchingCubesProject;
 using UnityEngine;
 
-public enum INITIAL_DIRECTION { UP, DOWN };
-
-public class LSystemEditor : MonoBehaviour
+namespace LSystem
 {
-    [Header("Default Settings")]
-    [Tooltip("Default forward distance")]
-    public int m_Distance = 10;
-    [Tooltip("Default turning angle")]
-    public short m_Angle = 90;
-
-    [Tooltip("Initial direction the turtle faces")]
-    public INITIAL_DIRECTION m_InitialDirection = INITIAL_DIRECTION.UP;
-
-    [Tooltip("Default segment thickness")]
-    public float m_Thickness = 2f;
-
-    [Tooltip("Default number of cross sections")]
-    public uint m_CrossSections = 4;
-    [Tooltip("Default number of cross section divisions")]
-    public uint m_CrossSectionDivisions = 2;
-
-    [Header("L-System")]
-    [Tooltip("Initial string to evaluate")]
-    public string m_StartString = "FF[+F][-F]F(42)";
-    [Tooltip("Number of iterations to apply the replacement rules")]
-    public uint m_Iterations = 0;
-    [Tooltip("Replacement rules")]
-    public string[] m_Rules = { "F=F+F-F-F+F" };
-
-    private Dictionary<char, string> ParseRuleInput(string[] rules)
+    public enum INITIAL_DIRECTION { UP, DOWN, LEFT, RIGHT, FORWARD, BACK, DIAGONAL };
+    public class LSystemEditor : MonoBehaviour
     {
-        var nt = new Dictionary<char, string>();
-        foreach (var r in rules)
-        {
-            //check syntax
-            if (r.Length < 3) throw new ArgumentException("Rule is in a wrong format");
-            if (!r.Contains('=')) throw new ArgumentException("Rule needs to include an =");
-            // search index of '='
-            var end = r.IndexOf('=');
-            var non_terminal = r[..end];
-            //TODO not necessarily: non terminals may only be one character
-            if (non_terminal.Length > 1) throw new ArgumentException("Non Terminals may only be exactly one character.");
-            var replacement = r[(end + 1)..];
-            // check if non_terminal is already contained or a terminal has the same symbol
-            if (nt.ContainsKey(non_terminal[0]) || LSystem.TERMINALS.Contains(non_terminal[0])) throw new ArgumentException("Cannot add a rule with a symbol that is already in use " + non_terminal[0]);
-            else nt.Add(non_terminal[0], replacement);
-        }
-        return nt;
-    }
+        [Header("Default Settings")]
+        [Tooltip("Default forward distance")]
+        public int m_Distance = 10;
+        [Tooltip("Default turning angle")]
+        public short m_Angle = 90;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        LSystem l = new(m_Distance, m_Angle, m_CrossSections, m_CrossSectionDivisions);
-        // var s = l.Parse("F", 1, NT);
-        // Debug.Log(s);
-        // var t = l.Tokenize(m_StartString);
-        // var v = l.Turtle3D(t);
-        // LSystem.PrintList(v);
-        var rules = ParseRuleInput(m_Rules);
-        Debug.Log(string.Join(Environment.NewLine, rules.Select(kvp => kvp.Key + ": " + kvp.Value.ToString())));
-        var output = l.Evaluate(m_StartString, m_Iterations, rules, true);
+        [Tooltip("Initial direction the turtle faces")]
+        public INITIAL_DIRECTION m_InitialDirection = INITIAL_DIRECTION.DOWN;
 
-        Segment[] segments = new Segment[output.Count];
-        for (int i = 0; i < output.Count; i++)
+        [Tooltip("Default segment thickness")]
+        public float m_Thickness = 2f;
+
+        [Tooltip("Default number of cross sections")]
+        public uint m_CrossSections = 4;
+        [Tooltip("Default number of cross section divisions")]
+        public uint m_CrossSectionDivisions = 2;
+
+        [Header("L-System")]
+        [Tooltip("Initial string to evaluate")]
+        public string m_StartString = "FF[+F][-F]F(42)";
+        [Tooltip("Number of iterations to apply the replacement rules")]
+        public uint m_Iterations = 0;
+        [Tooltip("Replacement rules")]
+        public string[] m_Rules = { "F=F+F-F-F+F" };
+
+        private Dictionary<char, string> ParseRuleInput(string[] rules)
         {
-            segments[i] = new Segment(output[i].Item1, output[i].Item2, m_Thickness);
-            Debug.Log(segments[i].startPoint + ", " + segments[i].endPoint);
+            var nt = new Dictionary<char, string>();
+            foreach (var r in rules)
+            {
+                //check syntax
+                if (r.Length < 3) throw new ArgumentException("Rule is in a wrong format");
+                if (!r.Contains('=')) throw new ArgumentException("Rule needs to include an =");
+                // search index of '='
+                var end = r.IndexOf('=');
+                var non_terminal = r[..end];
+                //TODO not necessarily: non terminals may only be one character
+                if (non_terminal.Length > 1) throw new ArgumentException("Non Terminals may only be exactly one character.");
+                var replacement = r[(end + 1)..];
+                // check if non_terminal is already contained or a terminal has the same symbol
+                if (nt.ContainsKey(non_terminal[0]) || LSystem.TERMINALS.Contains(non_terminal[0])) throw new ArgumentException("Cannot add a rule with a symbol that is already in use " + non_terminal[0]);
+                else nt.Add(non_terminal[0], replacement);
+            }
+            return nt;
         }
 
+        // Start is called before the first frame update
+        void Start()
+        {
+            LSystem l = new(m_Distance, m_Angle, m_CrossSections, m_CrossSectionDivisions, m_InitialDirection);
+            // var s = l.Parse("F", 1, NT);
+            // Debug.Log(s);
+            // var t = l.Tokenize(m_StartString);
+            // var v = l.Turtle3D(t);
+            // LSystem.PrintList(v);
+            var rules = ParseRuleInput(m_Rules);
+            Debug.Log(string.Join(Environment.NewLine, rules.Select(kvp => kvp.Key + ": " + kvp.Value.ToString())));
+            var output = l.Evaluate(m_StartString, m_Iterations, rules, true);
 
-        // Vector3 startPoint = new Vector3(-1, -3, 0);
-        // Vector3 endPoint = new Vector3(14, 0, 1.5f);
-        // Segment[] segments = new Segment[] { new Segment(startPoint, endPoint, 2) };
-        Metaball m = Metaball.BuildFromSegments(segments);
+            Segment[] segments = new Segment[output.Count];
+            for (int i = 0; i < output.Count; i++)
+            {
+                segments[i] = new Segment(output[i].Item1, output[i].Item2, m_Thickness);
+                Debug.Log(segments[i].startPoint + ", " + segments[i].endPoint);
+            }
 
 
-        //generate mesh from the metaball
-        MeshGenerator meshGen = GetComponent<MeshGenerator>();
-        //attributes such as the size and grid resolution can be set via component or through meshGen.gridResolution
-        meshGen.size = 100;
-        meshGen.gridResolution = 64;
+            // Vector3 startPoint = new Vector3(-1, -3, 0);
+            // Vector3 endPoint = new Vector3(14, 0, 1.5f);
+            // Segment[] segments = new Segment[] { new Segment(startPoint, endPoint, 2) };
+            Metaball m = Metaball.BuildFromSegments(segments);
 
-        meshGen.Generate(m);
+
+            //generate mesh from the metaball
+            MeshGenerator meshGen = GetComponent<MeshGenerator>();
+            //attributes such as the size and grid resolution can be set via component or through meshGen.gridResolution
+            meshGen.size = 100;
+            meshGen.gridResolution = 64;
+
+            meshGen.Generate(m);
+        }
+
+        // // Update is called once per frame
+        // void Update()
+        // {
+
+        // }
     }
-
-    // // Update is called once per frame
-    // void Update()
-    // {
-
-    // }
 }

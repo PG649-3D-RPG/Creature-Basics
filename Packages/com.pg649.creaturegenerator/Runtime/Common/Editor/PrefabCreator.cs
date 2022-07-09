4eb79a7520e582fa20020c0f31f43d0210af9b07
kb1t <kb1t@users.noreply.github.com>
2022-07-09 13:39:49 +0200

    + prefab creator controls + save without file dialog + lsystem export switch

--- Packages/com.pg649.creaturegenerator/Runtime/Common/Editor/PrefabCreator.cs
+++ Packages/com.pg649.creaturegenerator/Runtime/Common/Editor/PrefabCreator.cs
@@ -9,9 +9,109 @@ using UnityEngine;
 /// </summary>
 public class PrefabCreator : MonoBehaviour
 {
+    [Tooltip("Exported prefab filename. Defaults to GameObject's name.")]
+    public string creatureExportName = "";
+    [Tooltip("Path where the prefab will be saved. Must end with a \"/\". Defaults to \"Packages/com.pg649.creaturegenerator/Runtime/SkeletonFromLSystem/Prefabs/\".")]
+    public string creatureExportPath = "";
+
+    [Tooltip("If true exports lsystem json to the same directory.")]
+    public bool exportLSystemSettings = true;
+
     // Creates a new menu item 'PG649 > Create Prefab' in the main menu.
-    [MenuItem("PG649/Create Prefab")]
+    [MenuItem("PG649/Save Prefab")]
     private static void CreatePrefab()
+    {
+        // Keep track of the currently selected GameObject
+        GameObject gameObject = Selection.activeObject as GameObject;
+        if (gameObject == null)
+        {
+            EditorUtility.DisplayDialog(
+                            "Select GameObject",
+                            "Thou shalt select a gameobject!",
+                            "Sure thing!");
+            return;
+        }
+
+        string path = "";
+        string name = "";
+        if (gameObject.TryGetComponent(out PrefabCreator creator))
+        {
+            path = creator.creatureExportPath;
+            name = creator.creatureExportName;
+        }
+
+        // set default values if custom values were not set
+        if (path == "") path = "Packages/com.pg649.creaturegenerator/Runtime/SkeletonFromLSystem/Prefabs/";
+        if (name == "") name = gameObject.name;
+
+        string localPathToPrefabFolder = Path.Combine(path, name);
+        // Create folder if it does not exist and set path for exported prefab.
+        if (!Directory.Exists(localPathToPrefabFolder)) CreateFoldersRecursively(localPathToPrefabFolder);
+
+        string localPathToPrefab = Path.Combine(localPathToPrefabFolder, name + ".prefab");
+        // Make sure the file name is unique, in case an existing Prefab has the same name.
+        localPathToPrefab = AssetDatabase.GenerateUniqueAssetPath(localPathToPrefab);
+
+
+        var projectpath = Application.dataPath[..Application.dataPath.LastIndexOf("/")];
+        string globalPathToFolder = Path.Combine(projectpath, localPathToPrefabFolder);
+
+        string globalPathToLSystem = Path.Combine(globalPathToFolder, name + " lsystem.json");
+        string localPathToLSystem = globalPathToLSystem[(projectpath.Length + 1)..];
+        localPathToLSystem = AssetDatabase.GenerateUniqueAssetPath(localPathToLSystem);
+        globalPathToLSystem = Path.Combine(projectpath, localPathToLSystem);
+
+        if (!globalPathToFolder.StartsWith(projectpath))
+        {
+            EditorUtility.DisplayDialog(
+                        "Wrong Prefab path",
+                        "Thou shalt select a path below the project root!",
+                        "Sure thing!");
+            return;
+        }
+
+        // remove unneeded components and lock lsystem
+        if (gameObject.TryGetComponent(out SkeletonTest skeletonTest)) DestroyImmediate(skeletonTest);
+        // if (gameObject.TryGetComponent(out MarchingCubesProject.MeshGenerator meshGen)) DestroyImmediate(meshGen);
+        if (gameObject.TryGetComponent(out LSystem.LSystemEditor editor))
+        {
+            var prop = editor.GenerateProperties();
+            gameObject.AddComponent<LSystem.LSystemPropertyViewer>();
+            gameObject.GetComponent<LSystem.LSystemPropertyViewer>().Populate(prop);
+
+            // export lsystem
+            if (creator.exportLSystemSettings)
+            {
+                var jsonData = JsonUtility.ToJson(prop);
+                StreamWriter writer = new(globalPathToLSystem, false);
+                writer.WriteLine(jsonData);
+                writer.Close();
+                Debug.Log("JSON was saved successfully at: " + globalPathToLSystem);
+                AssetDatabase.ImportAsset(localPathToLSystem);
+            }
+            // remove LSystemEditor component
+            DestroyImmediate(editor);
+        }
+        if (gameObject.TryGetComponent(out PrefabCreator prefabCreator)) DestroyImmediate(prefabCreator);
+
+        // Create the new Prefab and log whether Prefab was saved successfully.
+        PrefabUtility.SaveAsPrefabAsset(gameObject, localPathToPrefab, out bool prefabSuccess);
+        if (prefabSuccess == true)
+            Debug.Log("Prefab was saved successfully at: " + localPathToPrefab);
+        else
+            Debug.Log("Prefab failed to save" + prefabSuccess);
+    }
+
+    // Disable the menu item if no selection is in place or editor is not in play mode.
+    [MenuItem("PG649/Save Prefab", true)]
+    private static bool ValidateCreatePrefab()
+    {
+        return Selection.activeGameObject != null && !EditorUtility.IsPersistent(Selection.activeGameObject) && EditorApplication.isPlaying;
+    }
+
+    // Creates a new menu item 'PG649 > Create Prefab' in the main menu.
+    [MenuItem("PG649/Save Prefab As")]
+    private static void CreatePrefabAs()
     {
         // Keep track of the currently selected GameObject
         GameObject gameObject = Selection.activeObject as GameObject;
@@ -42,29 +142,47 @@ public class PrefabCreator : MonoBehaviour
         }
         var localPath = path[(projectpath.Length + 1)..];
 
+        string globalPathToLSystem = Path.ChangeExtension(path, ".json");
+        string localPathToLSystem = globalPathToLSystem[(projectpath.Length + 1)..];
+
         // remove unneeded components and lock lsystem
         if (gameObject.TryGetComponent(out SkeletonTest skeletonTest)) DestroyImmediate(skeletonTest);
         // if (gameObject.TryGetComponent(out MarchingCubesProject.MeshGenerator meshGen)) DestroyImmediate(meshGen);
-        if (gameObject.TryGetComponent(out PrefabCreator prefabCreator)) DestroyImmediate(prefabCreator);
         if (gameObject.TryGetComponent(out LSystem.LSystemEditor editor))
         {
             var prop = editor.GenerateProperties();
             gameObject.AddComponent<LSystem.LSystemPropertyViewer>();
             gameObject.GetComponent<LSystem.LSystemPropertyViewer>().Populate(prop);
+
+            if (gameObject.TryGetComponent(out PrefabCreator prefabCreator))
+            {
+                if (prefabCreator.exportLSystemSettings)
+                {
+                    var jsonData = JsonUtility.ToJson(prop);
+                    StreamWriter writer = new(globalPathToLSystem, false);
+                    writer.WriteLine(jsonData);
+                    writer.Close();
+                    Debug.Log("JSON was saved successfully at: " + globalPathToLSystem);
+                    AssetDatabase.ImportAsset(localPathToLSystem);
+                }
+                DestroyImmediate(prefabCreator);
+            }
             DestroyImmediate(editor);
         }
+        // remove prefabCreator if it has not been removed before
+        if (gameObject.TryGetComponent(out PrefabCreator creator)) DestroyImmediate(creator);
 
         // Create the new Prefab and log whether Prefab was saved successfully.
         PrefabUtility.SaveAsPrefabAsset(gameObject, localPath, out bool prefabSuccess);
         if (prefabSuccess == true)
-            Debug.Log("Prefab was saved successfully");
+            Debug.Log("Prefab was saved successfully at: "+ localPath);
         else
             Debug.Log("Prefab failed to save" + prefabSuccess);
 
     }
     // Disable the menu item if no selection is in place or editor is not in play mode.
-    [MenuItem("PG649/Create Prefab", true)]
-    private static bool ValidateCreatePrefab()
+    [MenuItem("PG649/Save Prefab As", true)]
+    private static bool ValidateCreatePrefabAs()
     {
         return Selection.activeGameObject != null && !EditorUtility.IsPersistent(Selection.activeGameObject) && EditorApplication.isPlaying;
     }
@@ -88,4 +206,5 @@ public class PrefabCreator : MonoBehaviour
             if (!Directory.Exists(parent + "/" + folders[i])) AssetDatabase.CreateFolder(parent, folders[i]);
         }
     }
+
 }

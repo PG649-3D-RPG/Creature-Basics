964f7475e57283bad6c778587e356a74a129feb6
Markus Muegge <markus.muegge@t-online.de>
2022-08-04 18:41:37 +0200

    Add tooltips and spacing to settings inspectors

--- Packages/com.pg649.creaturegenerator/Runtime/SettingsObjects/CreatureGeneratorSettings.cs
+++ Packages/com.pg649.creaturegenerator/Runtime/SettingsObjects/CreatureGeneratorSettings.cs
@@ -21,35 +21,48 @@ public class CreatureGeneratorSettings : ScriptableObject
 public class SkeletonSettings
 {
     [Header("Bones")]
+    
+    [Tooltip("If set, the hip is a singular bone, instead of multiple. Only applies to the LSystem Generator.")]
     public bool ConnectHips = false;
+    
     [Header("Physics")]
+    
+    [Tooltip("Drag of each Rigidbody in the skeleton.")]
     public float RigidbodyDrag = 0.05f;
+    [Tooltip("Collision Detection Mode of each Rigidbody in the Skeleton.")]
     public CollisionDetectionMode CollisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
     
     [Header("Rendering")]
+    
+    [Tooltip("Determines how the primitive mesh casts shadows. Primitive mesh can be enabled in the Debug Settings.")]
     public ShadowCastingMode PrimitiveMeshShadows = ShadowCastingMode.Off;
 }
 
 [System.Serializable]
 public class DebugSettings
 {
+    [Tooltip("If set, bones are not affected by gravity.")]
     public bool DisableBoneGravity = false;
-    
+    [Tooltip("If set, physics simulation is stopped after creature generation. Allows inspecting result in its default pose.")]
     public bool DisablePhysics = false;
-
+    [Tooltip("If set, capsule meshes matching the bone colliders will be added to the skeleton.")]
     public bool AttachPrimitiveMesh = false;
-
-    public bool DrawMeshNormals = false;
 }
 
 [System.Serializable]
 public class MeshSettings
 {
+    [Tooltip("If set, a single mesh will be generated around the skeleton.")]
     public bool GenerateMetaballMesh = false;
+    [Tooltip("Choose whether the marching cubes algorithm should sample space via cubes or tetrahedons. Tetrahedons will better approximate the isosurface, but generate more vertices.")]
     public MeshGenerator.MARCHING_MODE Mode = MeshGenerator.MARCHING_MODE.CUBES;
+    [Tooltip("Choose the default material of the generated mesh.")]
     public Material Material;
+    [Tooltip("If set, surface normals will be smoothed across triangles. If not set, surface normals will be discontinous along triangle edges.")]
     public bool SmoothNormals = true;
+    [Tooltip("If set, surface normals will be visualized as vectors.")]
     public bool DrawNormals = false;
+    [Tooltip("If set, enabled the dual quaternion skinner.")]
     public bool enableDQSkinner = false;
     public int GridResolution = 90;
     public float Size = 20f;
--- Packages/com.pg649.creaturegenerator/Runtime/SettingsObjects/ParametricCreatureSettings.cs
+++ Packages/com.pg649.creaturegenerator/Runtime/SettingsObjects/ParametricCreatureSettings.cs
@@ -6,31 +6,42 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ParametricCreatureSettings", menuName = "PG649-CreatureGenerator/Parametric Creature Settings")]
 public class ParametricCreatureSettings : ScriptableObject
 {
+    [Header("Legs")]
     public float minLegThickness = 0.1f;
     public float maxLegThickness = 0.5f;
+    [Space(10)]
     public float minLegSize = 2f;
     public float maxLegSize = 6f;
 
+    [Header("Arms")]
     public float minArmThickness = 0.1f;
     public float maxArmThickness = 0.5f;
+    [Space(10)]
     public float minArmSize = 2f;
     public float maxArmSize = 6f;
 
+    [Header("Torso")]
     public float minTorsoSize = 4f;
     public float maxTorsoSize = 8f;
+    [Space(10)]
     public float minTorsoThickness = 0.4f;
     public float maxTorsoThickness = 1.5f;
 
+    [Header("Neck")]
     public float minNeckSize = 1f;
     public float maxNeckSize = 3f;
+    [Space(10)]
     public int minNeckSegments = 1;
     public int maxNeckSegments = 4;
 
+    [Header("Head")]
     public float minHeadSize = 0.3f;
     public float maxHeadSize = 1.5f;
 
+    [Header("Snout")]
     public float minSnoutSize = 1.2f;
     public float maxSnoutSize = 4f;
+    [Space(10)]
     public float minSnoutThickness = 0.1f;
     public float maxSnoutThickness = 0.5f;
 }

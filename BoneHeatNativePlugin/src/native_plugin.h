f709d7ef02ace2db2048bb9468dc4a00c4e6587a
Mathieu Herkersdorf <74296858+MathieuHerk@users.noreply.github.com>
2022-10-05 11:46:10 +0200

    Dll für Windows
    Dll und changes to native_plugin.h

--- BoneHeatNativePlugin/src/native_plugin.h
+++ BoneHeatNativePlugin/src/native_plugin.h
@@ -1,11 +1,6 @@
 #if defined(_MSC_VER)
-#ifdef NativePlugin_EXPORTS
 /* We are building this library */
 #   define NATIVEPLUGIN_API extern "C" __declspec(dllexport)
-#else
-/* We are using this library */
-#   define NATIVEPLUGIN_API extern "C" __declspec(dllimport)
-#endif
 #else
     #define NATIVEPLUGIN_API extern "C"
 #endif

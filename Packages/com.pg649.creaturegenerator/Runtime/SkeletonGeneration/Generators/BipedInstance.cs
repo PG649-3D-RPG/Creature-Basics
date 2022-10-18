3161326d05349d1f7208618992fce2a70d448c91
Markus Muegge <markus.muegge@t-online.de>
2022-10-18 13:14:02 +0200

    Change ParametricCreatureSettings to use new probability distributions

--- Packages/com.pg649.creaturegenerator/Runtime/SkeletonGeneration/Generators/BipedInstance.cs
+++ Packages/com.pg649.creaturegenerator/Runtime/SkeletonGeneration/Generators/BipedInstance.cs
@@ -6,18 +6,20 @@ public class BipedInstance : ISettingsInstance
     [ObservationOrder(0)] public readonly float ShoulderThickness;
     [ObservationOrder(1)] public readonly float ShoulderLength;
 
+    [ObservationOrder(19)] public readonly int NumArmBones;
     [ObservationOrder(2)] public readonly List<float> ArmThicknesses;
     [ObservationOrder(3)] public readonly List<float> ArmLengths;
 
     [ObservationOrder(4)] public readonly float HandRadius;
 
+    [ObservationOrder(18)] public readonly int NumLegBones;
     [ObservationOrder(5)] public readonly List<float> LegLengths;
     [ObservationOrder(6)] public readonly List<float> LegThicknesses;
 
     [ObservationOrder(7)] public readonly float FeetWidth;
     [ObservationOrder(8)] public readonly float FeetLength;
 
-    [ObservationOrder(17)] public readonly int NumTorsoSegments;
+    [ObservationOrder(17)] public readonly int NumTorsoBones;
     [ObservationOrder(9)] public readonly List<float> TorsoThicknesses;
     [ObservationOrder(10)] public readonly List<float> TorsoLengths;
 
@@ -30,58 +32,45 @@ public class BipedInstance : ISettingsInstance
     [ObservationOrder(15)] public readonly float HipThickness;
     [ObservationOrder(16)] public readonly float HipLength;
 
-    private static (List<float>, List<float>) InstanceLimb(ParametricCreatureSettings settings)
-    {
-        var totalLength = Random.Range(settings.minimumTotalArmLength, settings.maximumTotalArmLength);
-        var split = Random.Range(0.25f * totalLength, 0.75f * totalLength);
-        List<float> lengths = new() { split, totalLength - split };
-        var thicknesses =
-            InstanceUtils.ClampedLimbThicknesses(settings.minimumArmThickness, settings.maximumArmThickness, lengths);
-        thicknesses.Sort();
-        return (lengths, thicknesses);
-    }
-
     public BipedInstance(ParametricCreatureSettings settings, int? seed)
     {
         if (seed.HasValue) {
             Random.InitState(seed.Value);
         }
-        var (armLengths, armThicknesses) = InstanceLimb(settings);
-        var (legLengths, legThicknesses) = InstanceLimb(settings);
-        var (_, torsoLengths, torsoThicknesses, numTorsoSegments) = InstanceUtils.InstanceTorso(settings);
         
-        var neckBones = Random.Range(settings.minimumNeckBones, settings.maximumNeckBones + 1);
-        var totalNeckLength = Random.Range(settings.minimumNeckLength, settings.maximumNeckLength);
-        var neckBoneLength = totalNeckLength / neckBones;
-        var neckThickness =
-            InstanceUtils.ClampedThickness(settings.minimumNeckThickness, settings.maximumNeckThickness, neckBoneLength);
-        
-        var headSize = Random.Range(settings.minimumHeadSize, settings.maximumHeadSize);
-        
-        var hipLength = Random.Range(settings.minimumHipLength, settings.maximumHipLength);
-        var hipThickness = InstanceUtils.ClampedThickness(settings.minimumHipThickness, settings.maximumHipThickness, hipLength);
+        var numArmBones = settings.ArmBones.Sample();
+        var armLengths = settings.ArmLength.Samples(numArmBones);
+        var armThicknesses = settings.ArmThickness.Samples(numArmBones);
+        armThicknesses.Sort();
 
-        var shoulderLength = Random.Range(settings.minimumShoulderLength, settings.maximumShoulderLength);
-        var shoulderThickness = InstanceUtils.ClampedThickness(settings.minimumShoulderThickness, settings.maximumShoulderThickness,
-            shoulderLength);
+        var numLegBones = settings.LegBones.Sample();
+        var legLengths = settings.LegLength.Samples(numLegBones);
+        var legThicknesses = settings.LegThickness.Samples(numLegBones);
+        legThicknesses.Sort();
 
-        ShoulderLength = shoulderLength;
-        ShoulderThickness = shoulderThickness;
+        var numTorsoBones = settings.TorsoBones.Sample();
+        var torsoLengths = settings.TorsoLength.Samples(numTorsoBones);
+        var torsoThicknesses = settings.TorsoThickness.Samples(numTorsoBones);
+        
+        ShoulderLength = settings.ShoulderLength.Sample();
+        ShoulderThickness = settings.ShoulderThickness.Sample();
+        NumArmBones = numArmBones;
         ArmLengths = armLengths;
         ArmThicknesses = armThicknesses;
-        HandRadius = Random.Range(settings.minimumHandRadius, settings.maximumHandRadius);
+        HandRadius = settings.HandRadius.Sample();
+        NumLegBones = numLegBones;
         LegLengths = legLengths;
         LegThicknesses = legThicknesses;
-        FeetWidth = Random.Range(settings.minimumFeetWidth, settings.maximumFeetWidth);
-        FeetLength = Random.Range(settings.minimumFeetLength, settings.maximumFeetLength);
-        NumTorsoSegments = numTorsoSegments;
+        NumTorsoBones = numTorsoBones;
+        FeetWidth = settings.FeetWidth.Sample();
+        FeetLength = settings.FeetLength.Sample();
         TorsoThicknesses = torsoThicknesses;
         TorsoLengths = torsoLengths;
-        NeckBones = neckBones;
-        NeckBoneLength = neckBoneLength;
-        NeckThickness = neckThickness;
-        HeadSize = headSize;
-        HipLength = hipLength;
-        HipThickness = hipThickness;
+        NeckBones = settings.NeckBones.Sample();
+        NeckBoneLength = settings.NeckLength.Sample();
+        NeckThickness = settings.NeckThickness.Sample();
+        HeadSize = settings.HeadSize.Sample();
+        HipLength = settings.HipLength.Sample();
+        HipThickness = settings.HipThickness.Sample();
     }
 }

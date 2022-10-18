6bb35e62ef2d13302d37806aea635086a1f249a7
Markus Muegge <markus.muegge@t-online.de>
2022-10-18 13:11:54 +0200

    Add probability distributions

---
+++ Packages/com.pg649.creaturegenerator/Editor/DistributionDrawers.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(NormalDistribution))]
+public class NormalDistributionDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        EditorGUI.BeginProperty(position, label, property);
+
+        position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+        var muLabelRect = new Rect(position.x, position.y, position.width * 0.1f, position.height);
+        var muValueRect = new Rect(position.x + 0.1f * position.width, position.y, position.width * 0.4f,
+            position.height);
+        var sigmaLabelRect = new Rect(position.x + 0.5f * position.width, position.y, position.width * 0.1f,
+            position.height);
+        var sigmaValueRect = new Rect(position.x + 0.6f * position.width, position.y, position.width * 0.4f,
+            position.height);
+
+        var mu = property.FindPropertyRelative("Mean");
+        var sigma = property.FindPropertyRelative("StandardDeviation");
+
+        var style = new GUIStyle(EditorStyles.label)
+        {
+            alignment = TextAnchor.MiddleCenter,
+        };
+        
+        EditorGUI.LabelField(muLabelRect, "μ:", style);
+        mu.floatValue = EditorGUI.FloatField(muValueRect, mu.floatValue);
+        EditorGUI.LabelField(sigmaLabelRect, "σ:", style);
+        sigma.floatValue = EditorGUI.FloatField(sigmaValueRect, sigma.floatValue);
+
+        EditorGUI.EndProperty();
+    } 
+}
+
+[CustomPropertyDrawer(typeof(UniformIntegerDistribution))]
+public class UniformIntegerDistributionDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        EditorGUI.BeginProperty(position, label, property);
+
+        position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+        var minLabelRect = new Rect(position.x, position.y, position.width * 0.2f, position.height);
+        var minValueRect = new Rect(position.x + 0.2f * position.width, position.y, position.width * 0.3f,
+            position.height);
+        var maxLabelRect = new Rect(position.x + 0.5f * position.width, position.y, position.width * 0.2f,
+            position.height);
+        var maxValueRect = new Rect(position.x + 0.7f * position.width, position.y, position.width * 0.3f,
+            position.height);
+
+        var min = property.FindPropertyRelative("Min");
+        var max = property.FindPropertyRelative("Max");
+
+        var style = new GUIStyle(EditorStyles.label)
+        {
+            alignment = TextAnchor.MiddleCenter,
+        };
+        
+        EditorGUI.LabelField(minLabelRect, "Min:", style);
+        min.floatValue = EditorGUI.FloatField(minValueRect, min.floatValue);
+        EditorGUI.LabelField(maxLabelRect, "Max:", style);
+        max.floatValue = EditorGUI.FloatField(maxValueRect, max.floatValue);
+
+        EditorGUI.EndProperty();
+    } 
+}
---
+++ Packages/com.pg649.creaturegenerator/Runtime/SkeletonGeneration/Distributions.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+[Serializable]
+public class NormalDistribution
+{
+    public float Mean, StandardDeviation;
+
+    public NormalDistribution(float mean, float standardDeviation)
+    {
+        Mean = mean;
+        StandardDeviation = standardDeviation;
+    }
+    
+    private static float BoxMuller()
+    {
+        var u1 = Random.Range(0.0f, 1.0f);
+        var u2 = Random.Range(0.0f, 1.0f);
+
+        return Mathf.Sqrt(-2 * Mathf.Log(u1)) * Mathf.Cos(2 * Mathf.PI * u2);
+    }
+    
+    public float Sample()
+    {
+        var result = -1.0f;
+        do
+        {
+            result = Mean + BoxMuller() * StandardDeviation;
+        } while (result < 0.0f);
+
+        return result;
+    }
+
+    public List<float> Samples(int n)
+    {
+        var result = new List<float>();
+        for (var i = 0; i < n; i++)
+        {
+            result.Add(Sample()); 
+        }
+
+        return result;
+    }
+}
+
+[Serializable]
+public class UniformIntegerDistribution
+{
+    public int Min, Max;
+
+    public UniformIntegerDistribution(int min, int max)
+    {
+        Min = min;
+        Max = max;
+    }
+
+    public int Sample()
+    {
+        return Random.Range(Min, Max);
+    }
+    
+    public List<int> Samples(int n)
+    {
+        var result = new List<int>();
+        for (var i = 0; i < n; i++)
+        {
+            result.Add(Sample()); 
+        }
+
+        return result;
+    }
+}

af33d2fcea3bdfbc0d4e5b5efa91632a6c465e86
Jona <jonalukas@web.de>
2022-11-03 16:11:31 +0100

    ParametricCreatureSettings is now split into BipedSettings and QuadrupedSettings

---
+++ Packages/com.pg649.creaturegenerator/Runtime/SettingsObjects/BipedSettings.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "BipedSettings", menuName = "PG649-CreatureGenerator/Biped Settings")]
+public class BipedSettings : ParametricCreatureSettings
+{
+    [Header("Arms")]
+    public NormalDistribution ArmThickness = new(0.3f, 0.2f, 0.0f, float.MaxValue);
+    public NormalDistribution ArmLength = new(3.0f, 1.0f, 0.0f, float.MaxValue);
+    public UniformIntegerDistribution ArmBones = new(2, 2);
+    
+    [Header("Feet")]
+    public NormalDistribution FeetWidth = new(1.0f, 0.5f, 0.0f, float.MaxValue);
+    public NormalDistribution FeetLength = new(1.5f, 0.5f, 0.0f, float.MaxValue);
+    
+    [Header("Hands")]
+    public NormalDistribution HandRadius = new(0.75f, 0.25f, 0.0f, float.MaxValue);
+    
+    [Header("Shoulders")]
+    public NormalDistribution ShoulderThickness = new(0.3f, 0.2f, 0.0f, float.MaxValue);
+    public NormalDistribution ShoulderLength = new(0.75f, 0.25f, 0.0f, float.MaxValue);
+
+}
---
+++ Packages/com.pg649.creaturegenerator/Runtime/SettingsObjects/QuadrupedSettings.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "QuadrupedSettings", menuName = "PG649-CreatureGenerator/Quadruped Settings")]
+public class QuadrupedSettings : ParametricCreatureSettings
+{
+}

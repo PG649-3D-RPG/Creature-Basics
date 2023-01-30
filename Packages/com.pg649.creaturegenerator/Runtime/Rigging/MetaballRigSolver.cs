b6b81e96ae786af706fba91c0cddc184920a3a9a
Jona <jonalukas@web.de>
2023-01-30 20:04:05 +0100

    removed debug colors

--- Packages/com.pg649.creaturegenerator/Runtime/Rigging/MetaballRigSolver.cs
+++ Packages/com.pg649.creaturegenerator/Runtime/Rigging/MetaballRigSolver.cs
@@ -12,10 +12,8 @@ public class MetaballRigSolver : IRigSolver
         int nv = mesh.vertexCount;
         byte[] bonesPerVertex = new byte[nv];
         List<BoneWeight1> finalWeights = new List<BoneWeight1>();
-        Color[] colors = new Color[nv];
         for (int i=0; i<nv; i++)
         {
-            colors[i] = new Color(0,0,0);
             Bone[] vertexBones;
             float[] weights;
             (vertexBones, weights) = metaball.GetWeights(mesh.vertices[i].x, mesh.vertices[i].y, mesh.vertices[i].z);
@@ -29,14 +27,9 @@ public class MetaballRigSolver : IRigSolver
                 w.boneIndex = Array.IndexOf(bones, vertexBones[j]);
                 w.weight = weights[j] / sum;
                 finalWeights.Add(w);
-                if (vertexBones[j].category == BoneCategory.Shoulder)
-                    colors[i] += new Color(w.weight, 0, 0);
-                if (w.weight < 0)
-                    Debug.Log("aaaaaaaaaaaaaaaa");
             }
             bonesPerVertex[i] = (byte) weights.Length;
         }
-        mesh.colors = colors;
         mesh.SetBoneWeights(new NativeArray<byte>(bonesPerVertex, Allocator.Temp),
                             new NativeArray<BoneWeight1>(finalWeights.ToArray(), Allocator.Temp));
     }
